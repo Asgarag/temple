baaff99c0bbd1f4c4ec716aca092b75c439d11a0
Andrey <kozinandrew1@gmail.com>
2018-04-18 12:37:41 +0300

    Добавляет слайдер, расписание, пожертвование и цитату с вопросом

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,30 @@
 "use strict";
 
 var gulp = require('gulp'),
-		sass = require('gulp-sass'),
-		postcss = require('gulp-postcss'),
-		autoprefixer = require('autoprefixer'),
-		cssnano = require('cssnano'),
-		server = require("browser-sync"),
-		sourcemaps = require('gulp-sourcemaps'),
-		imagemin = require('gulp-imagemin'),
-		pngquant = require('imagemin-pngquant'),
-		del = require('del'),
-		runSequence = require('run-sequence').use(gulp),
-		svgstore = require('gulp-svgstore'),
-		svgmin = require('gulp-svgmin'),
-		cheerio = require('gulp-cheerio'),
-		pug = require('gulp-pug'),
-		rigger = require('gulp-rigger'),
-		rename = require('gulp-rename');
+	sass = require('gulp-sass'),
+	postcss = require('gulp-postcss'),
+	autoprefixer = require('autoprefixer'),
+	cssnano = require('cssnano'),
+	server = require("browser-sync"),
+	sourcemaps = require('gulp-sourcemaps'),
+	imagemin = require('gulp-imagemin'),
+	pngquant = require('imagemin-pngquant'),
+	del = require('del'),
+	runSequence = require('run-sequence').use(gulp),
+	svgstore = require('gulp-svgstore'),
+	svgmin = require('gulp-svgmin'),
+	cheerio = require('gulp-cheerio'),
+	pug = require('gulp-pug'),
+	rigger = require('gulp-rigger'),
+	plumber = require('gulp-plumber'),
+	rename = require('gulp-rename');
 
 gulp.task('layout', function () {
 	return gulp.src(['src/pages/*.pug'])
-			.pipe(pug({pretty: true}))
-			.pipe(gulp.dest('build'))
-			.pipe(server.reload({stream: true}));
+		.pipe(plumber())
+		.pipe(pug({pretty: true}))
+		.pipe(gulp.dest('build'))
+		.pipe(server.reload({stream: true}));
 });
 
 gulp.task('styles', function () {
@@ -32,61 +34,65 @@ gulp.task('styles', function () {
 		}), cssnano
 	];
 	return gulp.src('src/scss/style.scss')
-			.pipe(sourcemaps.init())
-			.pipe(sass())
-			.pipe(postcss(processors))
-			.pipe(sourcemaps.write())
-			.pipe(gulp.dest('build/css'))
-			.pipe(server.reload({stream: true}));
+		.pipe(plumber())
+		.pipe(sourcemaps.init())
+		.pipe(sass())
+		.pipe(postcss(processors))
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest('build/css'))
+		.pipe(server.reload({stream: true}));
 });
 
 gulp.task('images', function () {
 	return gulp.src(['src/images/**/*', '!src/images/svg', "!src/images/svg/*.svg"])
-			.pipe(imagemin({
-				progressive: true,
-				svgoPlugins: [{removeViewBox: false}],
-				use: [pngquant()]
-			}))
-			.pipe(gulp.dest('build/images'))
-			.pipe(server.reload({stream: true}));
+		.pipe(plumber())
+		.pipe(imagemin({
+			progressive: true,
+			svgoPlugins: [{removeViewBox: false}],
+			use: [pngquant()]
+		}))
+		.pipe(gulp.dest('build/images'))
+		.pipe(server.reload({stream: true}));
 });
 
 gulp.task('scripts', function () {
 	return gulp.src(['src/js/index.js'])
-			.pipe(rigger())
-			.pipe(gulp.dest('build/js'))
-			.pipe(server.reload({stream: true}));
+		.pipe(plumber())
+		.pipe(rigger())
+		.pipe(gulp.dest('build/js'))
+		.pipe(server.reload({stream: true}));
 });
 
 gulp.task('svgstore', function () {
-	return gulp
-			.src('src/images/svg/*.svg')
-			.pipe(svgmin())
-			.pipe(cheerio({
-				run: function ($) {
-					$('[fill]').removeAttr('fill');
-					$('[style]').removeAttr('style');
-				},
-				parserOptions: {xmlMode: true}
-			}))
-			.pipe(rename({prefix: 'icon-'}))
-			.pipe(svgstore({inlineSvg: true}))
-			.pipe(cheerio({
-				run: function ($) {
-					$('svg').attr('style', 'display:none');
-				},
-				parserOptions: {xmlMode: true}
-			}))
-			.pipe(rename('sprite.svg'))
-			.pipe(gulp.dest('build/images'));
+	return gulp.src('src/images/svg/*.svg')
+		.pipe(plumber())
+		.pipe(svgmin())
+		.pipe(cheerio({
+			run: function ($) {
+				$('[fill]').removeAttr('fill');
+				$('[style]').removeAttr('style');
+			},
+			parserOptions: {xmlMode: true}
+		}))
+		.pipe(rename({prefix: 'icon-'}))
+		.pipe(svgstore({inlineSvg: true}))
+		.pipe(cheerio({
+			run: function ($) {
+				$('svg').attr('style', 'display:none');
+			},
+			parserOptions: {xmlMode: true}
+		}))
+		.pipe(rename('sprite.svg'))
+		.pipe(gulp.dest('build/images'));
 });
 
 gulp.task('fonts', function () {
 	return gulp.src([
 		'src/font/**/*'
 	])
-			.pipe(gulp.dest('build/font'))
-			.pipe(server.reload({stream: true}));
+		.pipe(plumber())
+		.pipe(gulp.dest('build/font'))
+		.pipe(server.reload({stream: true}));
 });
 
 gulp.task('clean', function () {
@@ -106,7 +112,7 @@ gulp.task('serve', ['layout', 'styles', 'scripts', 'images', 'fonts', 'svgstore'
 	});
 
 	gulp.watch('src/**/*.{scss,sass}', ['styles']);
-	gulp.watch(['src/**/pages/*.pug','src/**/pages/layout/*.pug'], ['layout']);
+	gulp.watch(['src/**/pages/*.pug', 'src/**/pages/layout/*.pug'], ['layout']);
 	gulp.watch(['src/**/*.js', 'src/**/parts/*.js'], ['scripts']);
 	gulp.watch(['src/**/*.+(jpg,png)', 'src/**/**/*.+(jpg,png)'], ['images']);
 	gulp.watch('src/**/*.+(woff,woff2)', ['fonts']);
--- src/js/parts/carousel.js
+++ src/js/parts/carousel.js
@@ -1,5 +1,15 @@
 $(".owl-carousel").owlCarousel({
-    items: 1,
-    autoplay: true,
-    loop: true
+	items: 1,
+	autoplay: true,
+	loop: true,
+	navText: ["",""],
+	responsiveClass: true,
+	responsive:{
+		0:{
+			nav: false
+		},
+		480:{
+			nav: true
+		}
+	}
 });
