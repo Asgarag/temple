946916581533bcc1fb8b7096a05461b6f94c9a1d
Andrey <kozinandrew1@gmail.com>
2018-04-19 12:51:52 +0300

    Сделаны новости и настоятели

--- build/js/index.js
+++ build/js/index.js
@@ -32,17 +32,19 @@ $(document).ready(function () {
 		$('.menu__list').removeClass('menu__list--active');
 	});
 	
-	$('.menu__list-hidden').slideUp();
-	$('.menu__link').click(function () {
-		if ($(this).next().next().hasClass('menu__icon-accordion--active')) {
-			$('.menu__list-hidden').slideUp().next().removeClass('menu__icon-accordion--active');
-		}
-		else {
-			$('.menu__list-hidden').slideUp().next().removeClass('menu__icon-accordion--active');
-			$(this).next().next().addClass('menu__icon-accordion--active');
-		}
-		$(this).next().stop().slideToggle();
-	});
+	if ($(window).width() < 950) {
+		$('.menu__list-hidden').slideUp();
+		$('.menu__link').click(function () {
+			if ($(this).next().next().hasClass('menu__icon-accordion--active')) {
+				$('.menu__list-hidden').slideUp().next().removeClass('menu__icon-accordion--active');
+			}
+			else {
+				$('.menu__list-hidden').slideUp().next().removeClass('menu__icon-accordion--active');
+				$(this).next().next().addClass('menu__icon-accordion--active');
+			}
+			$(this).next().stop().slideToggle();
+		});
+	}
 	$(".question__button").click(function () {
 		$(".modal").addClass('modal--active');
 		return false;
